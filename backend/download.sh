d1abeab73492353df4e478ec09c99f332d263839
Dinh Ha Pham <phamdinhha95@gmail.com>
2021-01-20 12:38:17 +0700

    Add download script

---
+++ backend/download.sh
@@ -0,0 +1,5 @@
+mkdir pretrained_model/
+cd pretrained_model/
+
+gdown 'https://drive.google.com/uc?export=download&id=1h598B5xKGdmnAAY-r7yRqadDFIyDoFzS'
+gdown 'https://drive.google.com/uc?export=download&id=1HilvbcwyIHcAoyYyDQlSppamOqKKCV9L'
---
+++ frontend/download.sh
@@ -0,0 +1,4 @@
+wget -O deepfashion.tar.gz https://husteduvn-my.sharepoint.com/:u:/g/personal/thanh_lt163705_sis_hust_edu_vn/EQ_uFyOgyk5JvzcjFrlOo6ABEkH1vxHNGH5ACnDIApS7Yw?download=1
+tar -zxvf deepfashion.tar.gz
+unzip img.zip
+rm deepfashion.tar.gz
